00b13b2d6e851ec6d2a162702601e485c811e307
MarcMouries <marc@mouries.net>
2022-05-20 17:29:59 -0400

    + completed status

--- src/x-snc-milestone/sample_data.js
+++ src/x-snc-milestone/sample_data.js
@@ -1,5 +1,5 @@
 export const DEFAULT_STEP_LIST = [
-    { "id": 1, "label": "Submission" }, 
+    { "id": 1, "label": "Submission", "status": "completed"}, 
     { "id": 2, "label": "Triage", "status": "current" }, 
     { "id": 3, "label": "Fulfillment" },
     { "id": 4, "label": "Validation" }
