c3eed03ed08f409147458ff1c88b1c6958378b80
Marc Mouries <marc@mouries.net>
2022-05-22 16:39:07 -0400

    removed status

--- src/x-snc-milestone/sample_data.js
+++ src/x-snc-milestone/sample_data.js
@@ -1,6 +1,6 @@
 export const DEFAULT_STEP_LIST = [
-    { "id": 1, "label": "Submission" }, 
-    { "id": 2, "label": "Triage", "status": "current" }, 
+    { "id": 1, "label": "Submission" },
+    { "id": 2, "label": "Triage" },
     { "id": 3, "label": "Fulfillment" },
     { "id": 4, "label": "Validation" }
 ]
