1771e49d185be04001ad07db9ecee89ad7a033e3
Marc Mouries <marc@mouries.net>
2023-11-14 08:25:49 -0500

    new currentStage

--- src/x-snc-milestone/properties.js
+++ src/x-snc-milestone/properties.js
@@ -1,20 +1,40 @@
-import { DEFAULT_STEP_LIST } from "./sample_data";
+import { DEFAULT_STAGE_LIST, DEFAULT_CURRENT_STAGE } from "./sample_data";
 
 export default {
-  items: {
+  mode : {
+    schema: { type: "string" },
+    default: "RECORD",
+    onChange(currentValue, previousValue, dispatch) {
+      //dispatch(customActions.INITIALIZE_MAP);
+      console.log("%c " + "Property stages changed", "font-weight:bold");
+      console.log(" previousValue : " + previousValue);
+      console.log(" currentValue  : " + currentValue);
+    },
+  },
+  stages: {
     required: true,
     schema: { type: "array" },
-    default: DEFAULT_STEP_LIST,
+    default: DEFAULT_STAGE_LIST,
+    onChange(currentValue, previousValue, dispatch) {
+      //dispatch(customActions.INITIALIZE_MAP);
+      console.log("%c " + "Property stages changed", "font-weight:bold");
+      console.log(" previousValue : " + previousValue);
+      console.log(" currentValue  : " + currentValue);
+    },
   },
-  current: {
+  currentStage: {
     required: true,
     schema: { type: "string" },
-    default: "Triage",
+    default: DEFAULT_CURRENT_STAGE,
+    onChange(currentValue, previousValue, dispatch) {
+      console.log("%c " + "Property currentStage changed", "font-weight:bold");
+      console.log(" previousValue : " + previousValue);
+      console.log(" currentValue  : " + currentValue);    },
   },
   table: {
-    default: "",
+    default: ""
   },
   sysId: {
-    default: "",
-  },
+    default: ""
+  }
 };
