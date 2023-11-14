ede4668e43412cde99bb95cadfbd863d47c1060e
Marc Mouries <marc@mouries.net>
2023-11-14 08:23:27 -0500

    flatten the stage list

--- src/x-snc-milestone/sample_data.js
+++ src/x-snc-milestone/sample_data.js
@@ -1,4 +1,10 @@
-export const DEFAULT_STEP_LIST = [
+export const DEFAULT_STAGE_LIST = ["Stage 1", "Stage 2", "Stage 3"];
+export const DEFAULT_CURRENT_STAGE = "Stage 2";
+
+export const DEFAULT_STAGE_LIST_2 = ["Submission", "Triage", "Fulfillment" , "Closure"];
+
+
+export const DEFAULT_STEP_LIST_ORIGIN = [
     { "id": 1, "label": "Submission" },
     { "id": 2, "label": "Triage" },
     { "id": 3, "label": "Fulfillment" },
