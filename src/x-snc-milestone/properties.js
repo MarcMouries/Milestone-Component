94fd78a39267eadda9feb1cc62362fa12411abc5
MarcMouries <Marc@Mouries.net>
2023-11-29 08:54:07 -0500

    currentStage update

--- src/x-snc-milestone/properties.js
+++ src/x-snc-milestone/properties.js
@@ -1,4 +1,5 @@
 import { DEFAULT_STAGE_LIST, DEFAULT_CURRENT_STAGE } from "./sample_data";
+import { CURRENT_STAGE_CHANGED } from "./constants";
 
 export default {
   mode : {
@@ -16,7 +17,6 @@ export default {
     schema: { type: "array" },
     default: DEFAULT_STAGE_LIST,
     onChange(currentValue, previousValue, dispatch) {
-      //dispatch(customActions.INITIALIZE_MAP);
       console.log("%c " + "Property stages changed", "font-weight:bold");
       console.log(" previousValue : " + previousValue);
       console.log(" currentValue  : " + currentValue);
@@ -26,10 +26,15 @@ export default {
     required: false,
     schema: { type: "string" },
     default: DEFAULT_CURRENT_STAGE,
-    onChange(currentValue, previousValue, dispatch) {
+    onChange(currentValue, previousValue, dispatch, updateState) {
       console.log("%c " + "Property currentStage changed", "font-weight:bold");
       console.log(" previousValue : " + previousValue);
-      console.log(" currentValue  : " + currentValue);    },
+      console.log(" currentValue  : " + currentValue);
+      console.log(" CALLING updateState with " + currentValue);
+
+      //dispatch(CURRENT_STAGE_CHANGED);
+      updateState({ currentStage: currentValue });
+    },
   },
   table: {
     required: false,
